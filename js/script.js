b10d343a792e648dd77aadf3508c42b819458b48
Alex Andstén <alex.andsten@hotmail.com>
2021-01-04 04:35:33 +0100

    a

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Globala konstanter och variabler
+//array för 1-40 brickor
+const allBricks 
+
+brickElem   //bricka som flyttas, kan byta class mellan front och back
+
+gameBoard   //planen där alla brickor ska placeras
